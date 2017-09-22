ca5be5964304e511b0bd6c275eea914d1998806c
jiangyuesong <1259012537@qq.com>
2017-09-22 10:00:27 +0800

    test

--- dtask/dtask/urls.py
+++ dtask/dtask/urls.py
@@ -25,7 +25,7 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
